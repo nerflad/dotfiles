b5ef70c70ccd6ad18d42cfbe25ebffa32b99168e
Eric Bailey <ericbaileydrums@gmail.com>
2016-10-10 00:30:17 -0400

    reformat long fish prompt line

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -65,5 +65,10 @@ function fish_prompt --description 'Write out the prompt'
 		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
 	end
 
-	echo -n -s (set_color $fish_color_autosuggestion) "[" "$USER" $normal @ "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (basename (pwd)) $normal (set_color F54D27) (__fish_vcs_prompt) (set_color $fish_color_autosuggestion) "]" $prompt_status (set_color $fish_color_autosuggestion) "\$ "
+	echo -n -s (set_color $fish_color_autosuggestion) "["\
+        "$USER" $normal @ "$__fish_prompt_hostname" $normal ' '\
+            (set_color $color_cwd) (basename (pwd)) $normal\
+            (set_color F54D27) (__fish_vcs_prompt)\
+            (set_color $fish_color_autosuggestion) "]" $prompt_status\
+            (set_color $fish_color_autosuggestion) "\$ "
 end
