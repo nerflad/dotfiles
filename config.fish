f8520b7f2a1662d3047f7f905b07061033ad10e8
Eric Bailey <ericbaileydrums@gmail.com>
2019-02-20 15:58:15 -0500

    fix variable exports

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
-set -U PATH $PATH '/home/elb/bin'
-set -U XDG_CACHE_HOME '/home/elb/backup/.cache'
+set PATH $PATH '/home/elb/bin'
+export XDG_CACHE_HOME '/home/elb/backup/.cache'
+export AURDEST '/home/elb/backup/.cache/pacaur'
 export EDITOR=vim
 export VISUAL=vim
 fish_default_key_bindings
