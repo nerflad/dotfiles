4fb6a9756a9be2bbe0cb2bdea144e82b5a6cac15
Eric Bailey <ericbaileydrums@gmail.com>
2019-05-14 04:34:11 -0400

    config.fish

--- config.fish
+++ config.fish
@@ -3,4 +3,5 @@ export XDG_CACHE_HOME='/home/elb/backup/.cache'
 export AURDEST='/home/elb/backup/.cache/pacaur'
 export EDITOR=vim
 export VISUAL=vim
+export BAT_THEME="zenburn"
 fish_default_key_bindings
