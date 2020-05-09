716e14c5ed638c90d0021be428cd406e8b9ff3a7
Eric Bailey <elb@ericbailey.org>
2020-05-09 12:48:54 -0400

    style refactor

--- install.sh
+++ install.sh
@@ -2,60 +2,79 @@
 
 # check depends
 which curl git sudo >/dev/null
+[ $? -gt 0 ] && (echo "One or more dependencies not met."; exit 1)
 
-if [ $? -gt 0 ]; then
-    echo "One or more dependencies is not met."
-    exit 1
-fi
+# TODO: switch to vim's new native package system
+# vim plugin list
+# structure: (username repo-name)
+declare -ar plugins=(
+    vim-airline     vim-airline
+    vim-airline     vim-airline-themes
+    qpkorr          vim-bufkill
+    jamessan        vim-gnupg
+    mhinz           vim-signify
+    pangloss        vim-javascript
+    mhinz           vim-startify
+    mustache        vim-mustache-handlebars
+);
+
+# symlinks
+# structure: (filename link-path)
+declare -ar symlinks=(
+    config.fish     ~/.config/fish/config.fish
+    vimrc           ~/.vimrc
+    screenrc        ~/.screenrc
+    solarized.vim   ~/.vim/colors/solarized.vim
+);
 
 
-echo Creating .vim directory tree if necessary...
-for i in .vim .vim/backup .vim/swap .vim/bundle .vim/autoload .vim/colors .vim/ftdetect .vim/ftplugin
-do
-    if ! [ -d ~/$i ]; then
-        mkdir ~/$i
-    fi
+# create vim user directory tree if needed
+for i in . backup swap bundle autoload colors ftdetect ftplugin
+do [ -d ~/.vim/$i ] || mkdir ~/.vim/$i; done
+
+
+# install pathogen
+[ -e ~/.vim/autoload/pathogen.vim ] ||\
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
+
+# clone/update vim plugins
+echo Cloning vim plugins...
+
+for ((i = 0 ; i <= $((${#plugins[@]} - 1)) ; i = i+2 )); do
+    user=${plugins[$i]}
+    repo=${plugins[$i+1]}
+    [ -d ~/.vim/bundle/$repo ] && \
+    (cd ~/.vim/bundle/$repo; git pull 1>/dev/null && echo ✅ $repo) && continue;
+    git clone https://github.com/$user/$repo ~/.vim/bundle/$repo
 done
-
-if ! [ -e ~/.vim/autoload/pathogen.vim ]; then
-    echo Installing pathogen...
-    curl -Lsso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-fi
-
-# download pathogen plugins
-pushd ~/.vim/bundle
-    git clone https://github.com/vim-airline/vim-airline 2>/dev/null || (pushd vim-airline; git pull; popd)
-    git clone https://github.com/vim-airline/vim-airline-themes 2>/dev/null || (pushd vim-airline-themes; git pull; popd)
-    git clone https://github.com/qpkorr/vim-bufkill 2>/dev/null || (pushd vim-bufkill; git pull; popd)
-    git clone https://github.com/jamessan/vim-gnupg 2>/dev/null || (pushd vim-gnupg; git pull; popd)
-    git clone https://github.com/mhinz/vim-signify 2>/dev/null || (pushd vim-signify; git pull; popd)
-    git clone https://github.com/pangloss/vim-javascript 2>/dev/null || (pushd vim-signify; git pull; popd)
-    git clone https://github.com/mhinz/vim-startify 2>/dev/null || (pushd vim-startify; git pull; popd)
-popd
+printf "\n"
 
 
-
-echo Creating /home symlinks...
+echo Creating home symlinks...
 for i in $(pwd)/fish/*.fish
 do
     file=`basename $i`
-    ln -s $i ~/.config/fish/functions/$file 2>/dev/null && echo ~/.config/fish/functions/$file
+    ln -s $i ~/.config/fish/functions/$file 2>/dev/null && \
+    (echo ~/.config/fish/functions/✅ $file)
 done
-ln -s $(pwd)/config.fish ~/.config/fish/config.fish 2>/dev/null && echo config.fish
-ln -s $(pwd)/vimrc ~/.vimrc 2>/dev/null && echo ~/.vimrc
-ln -s $(pwd)/screenrc ~/.screenrc 2>/dev/null && echo ~/.screenrc
 
-ln -s $(pwd)/solarized.vim ~/.vim/colors/solarized.vim 2>/dev/null
-
+for ((i = 0 ; i <= $((${#symlinks[@]} - 1)) ; i = i+2 )); do
+    file=${symlinks[i]}
+    path=${symlinks[i+1]}
+    [ -e $path ] && (echo ✅ $file) && continue
+    ln -s $(pwd)/$file $path && echo ✅ ${symlinks[i]}
+done
 
 read -p "Create privileged symlinks? (y/N): " choice
 case $choice in
     y|Y )
         sudo mv /etc/wikicurses.conf /etc/wikicurses.conf.bak
-        sudo ln -s $(pwd)/wikicurses.conf /etc/wikicurses.conf
+        sudo ln -s $(pwd)/wikicurses.conf /etc/wikicurses.conf \
+          && echo ✅ wikicurses.conf
 
         sudo ln -s $(pwd)/solarized.vim /usr/share/vim/vim82/colors/solarized.vim &&\
-            echo /usr/share/vim/vim82/colors/solarized.vim
+          echo ✅ /usr/share/vim/vim82/colors/solarized.vim
+
         read -p "Overwrite global vimrc, backing up original? (y/N): " choice
         case $choice in
             y|Y )
@@ -63,7 +82,7 @@ case $choice in
                     currenttime=`date +"%T"`
                     sudo mv /etc/vimrc /etc/vimrc.$currenttime.bak
                 fi
-                sudo ln -s $(pwd)/vimrc /etc/vimrc || echo /etc/vimrc;;
+                sudo ln -s $(pwd)/vimrc /etc/vimrc && echo ✅ /etc/vimrc;;
             * )
         esac;;
     * )
